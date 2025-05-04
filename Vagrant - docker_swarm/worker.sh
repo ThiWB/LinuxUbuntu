a73ceda118efb1eefef727a6b103699f5b05e4f2
Thiago Wurster Balbinot <168877969+ThiWB@users.noreply.github.com>
2025-05-04 13:09:50 -0300

    Update worker.sh

--- Vagrant - docker_swarm/worker.sh
+++ Vagrant - docker_swarm/worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3qbxk45ygknfb0nt54cg2xfd0uaghks9tl22ynewf31oh-cwa1wqjuccs0tkqg0q6l121 192.168.56.11:2377
+    docker swarm join --token SWMTKN-1-45ygknfb0nt54cg2xfd0uaghks9tl22ynewf31oh-cwa1wqjuccs1 192.168.56.11:2377
